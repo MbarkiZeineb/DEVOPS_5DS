7f8c912f99a34d49f6871db4722ce2c1dfb0f04f
AbirBarouni <98458486+AbirBarouni@users.noreply.github.com>
2023-10-26 19:33:40 +0100

    commit "delete fct"

--- DevOps_Project/src/main/java/tn/esprit/devops_project/services/StockServiceImpl.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/services/StockServiceImpl.java
@@ -30,6 +30,10 @@ public class StockServiceImpl implements IStockService {
 
         return stockRepository.findAll();
     }
+    @Override
+    public void deleteStock(Long id) {
+        stockRepository.deleteById(id);
+    }
 
 
 
